cf4d663f3f6cb18b9851630e90dc14eaaa52b3ec
jamshid0705 <jamshidshamshod0705@gmail.com>
2023-11-07 15:22:36 +0500

    cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CarModule],
   controllers: [AppController, UserController],
   providers: [AppService],
+  exports:[]
 })
 export class AppModule {}
