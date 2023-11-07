d097390be26dba498381c96499ac85c98710528c
jamshid0705 <jamshidshamshod0705@gmail.com>
2023-11-07 16:34:34 +0500

    crud

---
+++ src/user/dto/user.dto.ts.ts
@@ -0,0 +1,6 @@
+export class UserDto {
+    id:number
+    title:string
+    description:string
+    excerpt:string
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,35 @@
 import { Controller ,Get, HttpCode} from '@nestjs/common';
+import { UserDto } from './dto/user.dto.ts';
 
 @Controller('user')
 export class UserController {
+    users:UserDto[]
+    constructor(){
+        this.users=[
+            {
+                id:0,
+                title:'Nestjs',
+                description:"Nestjs full course from 0 to hero",
+                excerpt:'Nestjs full course'
+            },
+            {
+                id:1,
+                title:'Nextjs',
+                description:"Nextjs full course from 0 to hero",
+                excerpt:'Nextjs full course'
+            },
+            {
+                id:2,
+                title:'Nuxtjs',
+                description:"Nuxtjs full course from 0 to hero",
+                excerpt:'Nuxtjs full course'
+            }
+        ]
+    }
+
     @HttpCode(200)
     @Get()
     async getAll(){
-        return 
+        return this.users
     }
 }
